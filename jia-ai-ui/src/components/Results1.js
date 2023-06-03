f1d61ccdaa42e32f06ac057aba232614b7fe417a
McKenzie Bourn <bournm@oregonstate.edu>
2023-06-02 17:47:43 -0700

    styling

--- jia-ai-ui/src/components/Results1.js
+++ jia-ai-ui/src/components/Results1.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-
+import Typewriter from 'typewriter-effect';
 
 const Results1 = () => {
-  const location = useLocation();  // Using the useLocation hook to get the current location
-  const { recommendations } = location.state; // Destructuring the recommendations object from the current location state
-  console.log(recommendations); // represents the response from the AI 
+  const location = useLocation();
+  const { recommendations } = location.state;
   
+
   return (
     <div className="text-center mt-20">
-    <div className= "font-semibold text-3xl">Based on your search, we think you'd like:</div>
-    <br/>
-    <div className="text-lg">
-      {recommendations} 
+      <div className="font-semibold text-3xl">
+        <Typewriter
+         options={{
+          cursor: '', // Hide the cursor
+          delay: 50, // Delay between each character
+        }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString('Based on your search, we think you\'d like:')
+              .start();
+          }}
+        />
       </div>
-      <br/>
-    <div className="mt-20"><p>Click <Link to="/Homepage"  className="underline">here</Link> to return home.</p></div>
+      <br />
+      <div className="text-lg">
+        <Typewriter
+        options={{
+          cursor: '', // Hide the cursor
+          delay: 50, // Delay between each character
+        }}
+         onInit={(typewriter) => {
+          typewriter
+            .pauseFor(2800)
+            .typeString(recommendations)
+            .start();
+          }}
+        />
+      </div>
+      <br />
+      <div className="mt-20">
+        <p>
+          Click <Link to="/Homepage" className="underline">here</Link> to return home.
+        </p>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Results1;
